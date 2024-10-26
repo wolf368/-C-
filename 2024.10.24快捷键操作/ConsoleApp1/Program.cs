5a07fc06cdac28e25e872966679c7df8a4117ea6
wolf368 <2137315302@qq.com>
2024-10-26 16:48:38 +0800

    Add files via upload

---
+++ 2024.10.24快捷键操作/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        #region 冗余代码
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //    Console.ReadKey();
+        //    Console.WriteLine("Hello Everyone!");
+            #endregion
+        }
+}
+}
+//ctrl+K+D快速对齐代码
+//ctrl + K + C注释所选代码
+//    ctrl + K + U快速取消所选代码的注释
+//    ctrl + Z撤销操作
+//    shift + home / shift + end 光标到前 / 后列
+//    #region #endregion 折叠冗余代码
+//ctrl+save 快速保存
