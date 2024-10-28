aa26317c5bc9c3e4e79ae4cd23ad3c2e3c3c73ea
wolf368 <2137315302@qq.com>
2024-10-28 19:27:48 +0800

    Add files via upload

---
+++ 占位符与字符串/2024.10.28/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024._10._28
+{
+    class Class1
+    {
+        //占位符：先挖个坑，再填个坑。
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = 20;
+            int n3 = 30;
+            Console.WriteLine("第一个数字是{0},第二个数字是{1},第三个数字是{2} ",n1,n2,n3);
+        }
+
+    }
+}
---
+++ 占位符与字符串/2024.10.28/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024._10._28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "张三";
+            Console.WriteLine("你好," + name);
+            /*  +在这里起到连接作用，若两边为数字则起相加作用   */
+            Console.WriteLine(5 + 5);
+            Console.WriteLine(5 + "5"); //此处用引号则表示字符串
+            Console.ReadKey();                                                       
+        }
+    }
+}
+
+    // = 表示赋值的意思，有等号连接的表达式称为赋值表达式
+    //变量可重复赋值。老值不存在，被新值替换
+
+/*变量：存储数据
+ * 命名规范：double decimal string char
+ * Camel：
+ * Pascal：
+ */
+
