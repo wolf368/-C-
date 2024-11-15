8093cfb40e979c4686f1f2cd0e5fec8c56030701
wolf368 <2137315302@qq.com>
2024-11-15 10:14:04 +0800

    Add files via upload

---
+++ 三元表达式.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024._11._14
+{
+    class 三元表达式
+    {
+        /*
+        三元表达式
+        语法：
+            表达式1？表达式2：表达式3；
+            表达式1一般为一个关系表达式。
+            如果表达式1的值为true，那么表达式2的值就是整个三元表达式的值
+            如果表达式1的值为false，那么表达式3的值就是整个三元表达式的值
+            注意：2的结果类型必须跟3的结果类型一致，并且也要跟整个三元表达式的结果类型一致
+        可替代if类型
+        */
+        static void Main(string[] args)
+        {
+            //比较得最大数
+            Console.WriteLine("请输入第一个数字");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入第二个数字");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            int max = n1 > n2 ? n1 : n2;
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 随机数.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 随机数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //产生随机数
+            //1.创造能产生随机数的对象
+            Random r = new Random();
+            //放到循环外面，防止反复开辟空间，引起卡顿
+            while (true)
+            {
+                //让产生随机数的这个对象调用方法来产生随机数
+                int rNumber = r.Next(1, 11);
+                //左闭右开
+                Console.WriteLine(rNumber);
+                Console.ReadKey();
+                */
+            Random r = new Random();
+            while (true)
+            {
+                Console.WriteLine("请输入一个名字,我来判断他上辈子的死法");
+                string name = Console.ReadLine();
+                int rNumber = r.Next(1, 7);
+                if (name == "李仕尧")
+                {
+                    Console.WriteLine("李仕尧是腰缠弯管，在众人的祝福下善终的");
+                 }
+                else
+                {
+                    switch (rNumber)
+                    {
+                        case 1:
+                            Console.WriteLine("{0}上辈子是吃翔噎死的", name);
+                            break;
+                        case 2:
+                            Console.WriteLine("{0}上辈子是被飞机撞死的", name);
+                            break;
+                        case 3:
+                            Console.WriteLine("{0}上辈子是精尽而亡的", name);
+                            break;
+                        case 4:
+                            Console.WriteLine("{0}上辈子是踩到香蕉皮摔死的", name);
+                            break;
+                        case 5:
+                            Console.WriteLine("{0}上辈子是饿死的", name);
+                            break;
+                        case 6:
+                            Console.WriteLine("{0}上辈子是玩游戏累死的", name);
+                            break;
+                        case 7:
+                            Console.WriteLine("{0}上辈子是驴踹死的", name);
+                            break;
+
+                    }
+                }
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
+
